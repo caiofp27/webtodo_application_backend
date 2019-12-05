e96bf9620aee5456862eb295731b7f62ba309e05
Caio <caioymc@live.com>
2019-12-05 19:31:43 +0000

    Implemented final functions

--- handler.js
+++ handler.js
@@ -10,16 +10,11 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: 'todo_app'
 });
-
 const app = express();
-
 app.use(cors());
-
-//Allows Express to parse JSON data that is sent on the body of any request
 app.use(bodyParser.json());
-
 app.get("/tasks", function(request, response){
-  connection.query("SELECT * FROM tasks", function (err, data){
+  connection.query("SELECT * FROM tasks", function(err, data){
     if(err){
       response.status(500).json({error: err});
     }else{
@@ -27,11 +22,10 @@ app.get("/tasks", function(request, response){
     }
   })
 });
-
 app.delete("/tasks/:taskId", function(request, response){
   //Delete the task
   const taskId = request.params.taskId;
-  connection.query("DELETE from tasks WHERE taskId = ?", [taskId], function (err){
+  connection.query("DELETE from tasks WHERE taskId = ?", [taskId], function(err){
   if(err){
     response.status(500).json({error: err});
   }else{
@@ -39,27 +33,25 @@ app.delete("/tasks/:taskId", function(request, response){
   }
   });
 });
-
 app.post("/tasks", function(request, response){
   //Create a new task
   const task = request.body;
-  const q = "INSERT tasks SET userId = ?, taskText = ?, completed = ?";
-  connection.query(q, [task.userId, task.taskText, task.completed], function (err, data){
+  const q = "INSERT tasks SET taskText = ?, completed = ?";
+  connection.query(q, [task.taskText, task.completed], function(err, data){
     if(err){
       response.status(500).json({error: err});
     }else{
-      response.status(201).send("Created a new task with text: "+task.taskText);
+      response.status(201).json(data);
     }
   })
 });
-
 app.put("/tasks/:taskId",function (request, response){
   // Update task
-  const task = request.body;
   const taskId = request.params.taskId;
-  //expected {text, completed}
-  const q = "UPDATE tasks SET taskText = ?, completed = ? WHERE taskId = ?";
-  connection.query(q, [task.taskText, task.completed, taskId], function (err, data){
+  const task = request.body;
+  //expected {status completed}
+  const q = "UPDATE tasks SET completed = ? WHERE taskId = ?";
+  connection.query(q, [task.completed, taskId], function (err, data){
     if(err){
       response.status(500).json({error: err});
     }else{
